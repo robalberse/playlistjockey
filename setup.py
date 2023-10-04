bd105148de69b98e0b55af789f14fa49f6569a93
Robby Alberse <robalberse@gmail.com>
2023-10-04 16:28:02 -0400

    Adding license and setup files.

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="playlistjockey",
+    version="1.0.0",
+    description="Unlock innovative ways to experience playlists.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU GPLv3",
+        "Operating System :: OS Independent"
+    ]
+    python_requires=">=3.9",
+    py_modules=["playlistjockey"]
+)
